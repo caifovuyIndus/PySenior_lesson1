2151a0ae4ab2ef5b6f2763c695d4e26983785f94
EgorIlin <ilinegor310@gmail.com>
2024-10-19 18:01:34 +0300

    3 created class Marker

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 print('Hello world!')
+
+a = 2
+b = 'hello'
+c = True
+d = 2.2
+
+
+class Marker:
+    def __init__(self, color='black'):
+        self.color = color
+        self.weight = 1.22
+        self.price = 3
+        self.isPresent = True
+
+    def getinfo(self):
+        print(f'color:{self.color}-price{self.price}€')
+
+redMarker = Marker('red')
+print(redMarker.color)
+redMarker.getinfo()
+
+blackMarker = Marker()
+print(blackMarker.color)
+blackMarker.getinfo()
