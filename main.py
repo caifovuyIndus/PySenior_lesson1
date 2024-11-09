a4dd3c579ef4e98f7c47de022c60ad49d747b6c9
EgorIlin <ilinegor310@gmail.com>
2024-11-09 16:55:51 +0200

    4 created closure function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print(f'lesson7\n')
 
-numbers = [1, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print('before', numbers)
 
 # for i in range(len(numbers)):
@@ -8,13 +8,28 @@ print('before', numbers)
 #
 # print('after', numbers)
 
-def up_square(n):
-    if n % 2 != 0:
-        return n**2
-    return n
+
+
+def set_up_pow(value: int, result=0):
+    pow = value
+    result = 0
+    delim = 2
+
+
+    def up_pow(n):
+        if n % delim != result:
+            return n**pow
+        return n
+
+
+    return up_pow
 
 def check_n(n):
     return n >= 5
 
-numbers_square = [up_square(n) for n in numbers if check_n(n)]
+up_square = set_up_pow(2)
+up_cube = set_up_pow(3, 1)
+
+numbers_square = [up_cube(n) for n in numbers if check_n(n)]
 print('after', numbers_square)
+
