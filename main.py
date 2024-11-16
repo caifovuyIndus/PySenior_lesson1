52eaec1bafc8577c48898ed05a04f3b26bf9f51e
EgorIlin <ilinegor310@gmail.com>
2024-11-16 16:46:30 +0200

    2 parse result

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 print(f'lesson9\n')
-
 import requests
+from bs4 import BeautifulSoup
+
+data_site = requests.get('https://coinmarketcap.com/')
+print('site coin market status code', data_site.status_code)
+
+if data_site.status_code == 200:
+    print('coin market is connected')
+    soup = BeautifulSoup(data_site.text, features='html.parser')
+    soup_list = soup.find_all('div', {'class': 'sc-b3fc6b7-0 dzgUIj'})
+
+    for block in soup_list:
+        print('parse result', block)
