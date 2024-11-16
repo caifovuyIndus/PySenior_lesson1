da3c5899a40e16ba417fa8788002e835f2816bcd
EgorIlin <ilinegor310@gmail.com>
2024-11-16 15:05:29 +0200

    2 unittest

--- test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from main import *
-
-class MainTest(unittest.TestCase):
-    def test_sumv_value(self):
-        self.assertEqual(sum_value(2, 3), 6)
-
-if __name__ =='__main__':
-    unittest.main()
