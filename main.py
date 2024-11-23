b90b9d786a1b46f3cb5de4943c3b68e3915cf206
student <ilinegor310@gmail.com>
2024-11-23 17:25:57 +0200

    5 3 cat face rectangle

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print(f'lesson11\n')
 
 import cv2
 
-image_path = '394064.jpeg'
+image_path = '3xcats.jpg'
 
 handle_cat_face = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
 image = cv2.imread(image_path)
@@ -11,8 +11,12 @@ image = cv2.imread(image_path)
 cat_face_data = handle_cat_face.detectMultiScale(image)
 # print(cat_face_data)
 
+colors = [(0, 255, 255), (0, 255, 0), (0, 0, 255)]
+i = 0
 for (x, y, w, h) in cat_face_data:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 3)
+    cv2.rectangle(image, (x, y), (x+w, y+h), colors[i], 3)
+    i += 1
 
 cv2.imshow('cat',image)
 cv2.waitKey()
+cv2.rec
