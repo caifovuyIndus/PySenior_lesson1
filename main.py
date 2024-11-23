91cfc554b2908b51bf8678d08bf58df7811ba77c
student <ilinegor310@gmail.com>
2024-11-23 16:48:16 +0200

    3 cat face data

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ print(f'lesson11\n')
 import cv2
 
 image_path = '394064.jpeg'
+
+handle_cat_face = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
 image = cv2.imread(image_path)
+
+
+cat_face_data = handle_cat_face.detectMultiScale(image)
+print(cat_face_data)
+
+
+
 cv2.imshow('cat',image)
 cv2.waitKey()
